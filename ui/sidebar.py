215bd31f2784c7922c3b7fcb00f80a7ba1eb3ae6
Nandan Patel <nandankevan44@gmail.com>
2026-07-22 11:39:01 +0530

    _self -> _top

--- ui/sidebar.py
+++ ui/sidebar.py
@@ -350,7 +350,7 @@ def render_login_panel():
 
                 st.markdown(
                     f"""
-        <a href="{auth_url}" target="_self" style="
+        <a href="{auth_url}" target="_top" style="
             display: inline-block;
             width: 100%;
             text-align: center;
@@ -422,7 +422,7 @@ def render_login_panel():
 
             st.markdown(
         f"""
-        <a href="{auth_url}" target="_self" style="
+        <a href="{auth_url}" target="_top" style="
             display: inline-block;
             width: 100%;
             text-align: center;
